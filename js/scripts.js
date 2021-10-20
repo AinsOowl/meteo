0decabe0cbab712eecb7650850d502858d9a26e0
AinsOowl <elliot.van.bastelaere@gmail.com>
2021-10-20 08:30:10 +0200

    test java

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-console.log('ok');
+$(document).ready(function(){
+
+    $.ajax( {
+    
+    url:'https://prevision-meteo.ch/services/json/bruxelles-1',
+    method: "GET",
+    success:function(monObjet) {
+     console.log(monObjet);
+
+     $('#contenant').append('<div><h2>'+ monObjet +'</h2></div>')
+
+    } // success function
+    }); // ajax 
+    
+    }); // ready
