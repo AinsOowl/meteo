737ac035025dfea2ade95b8ff3be9bc1fa4327b3
AinsOowl <elliot.van.bastelaere@gmail.com>
2021-10-20 09:31:22 +0200

    elliot fonctionnel 1

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,17 @@ $(document).ready(function(){
     method: "GET",
     success:function(monObjet) {
      console.log(monObjet);
+     var element = document.getElementById("temps");
+         element.innerHTML = '<div><h1>'+ monObjet.fcst_day_1.tmin +'</h1><h2>'+ monObjet.fcst_day_1.day_long +'</h2><p>'+ monObjet.fcst_day_1.condition +'</p></div>';
 
-     $('#contenant').append('<div><h2>'+ monObjet +'</h2></div>')
+     //for(i=0; i<monObjet; i++){
+     //    console.log(monObjet[i]);
+    
 
+//$('#contenant').append('<div><h2>'+ monObjet[i] +'</h2></div>')
+
+
+//}
     } // success function
     }); // ajax 
     
