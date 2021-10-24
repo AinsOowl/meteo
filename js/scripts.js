62f9ec2b6bb6c37396bf9b6514952eb16b36dceb
D4SKO <92360209+D4SKO@users.noreply.github.com>
2021-10-25 00:26:18 +0200

    tim_final

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
      console.log(monObjet);
      //aujourd'hui
      var element = document.getElementById("auj");
-         element.innerHTML = '<div><img id="icon1" class="iconsauj" src="'+monObjet.current_condition.icon_big+'" alt="icon météo"><h1> <div class="dayname">'+ monObjet.fcst_day_1.day_long +'</div><br><div class"date">'+ monObjet.current_condition.date+'</div></h1><div class="nomville">'+ville+'</div><h2> <div class="ville"'+ville+'</div><br> <div class="tempact"> Temps actuelle: '+monObjet.current_condition.tmp+'</div><br> <div class="tempM"> Min/Max: '+ monObjet.fcst_day_1.tmin +'°/'+ monObjet.fcst_day_1.tmax+'°'+'</div></h2><p><div class="condition">Condition: '+ monObjet.current_condition.condition +'</div><br><div class="humidite">Humidité: '+monObjet.current_condition.humidity+'% </div><br><div class="dirvent">Direction du vent: '+monObjet.current_condition.wnd_spd+'m/s</div><div class="vitvent">Vitesse du vent: '+monObjet.current_condition.wnd_dir+'</div><div class="pression">Pression: '+monObjet.current_condition.pressure+' bars</div></p></div>';
+         element.innerHTML = '<div><img id="icon1" class="iconsauj" src="'+monObjet.current_condition.icon_big+'" alt="icon météo"><h1> <div class="dayname">'+ monObjet.fcst_day_1.day_long +'</div><br><div class"date">'+ monObjet.current_condition.date+'</div></h1><div class="nomville">'+ville+'</div><h2> <div class="ville"'+ville+'</div><br> <div class="tempact"> Temps actuelle: '+monObjet.current_condition.tmp+'</div><br> <div class="tempM"> Min/Max: '+ monObjet.fcst_day_1.tmin +'°/'+ monObjet.fcst_day_1.tmax+'°'+'</div></h2><p><div class="condition"><p>Condition: '+ monObjet.current_condition.condition +'</div><br><p><div class="humidite"><p>Humidité: '+monObjet.current_condition.humidity+'% </p></div><br><div class="dirvent"><p>Direction du vent: '+monObjet.current_condition.wnd_spd+'m/s</p></div><div class="vitvent"><p>Vitesse du vent: '+monObjet.current_condition.wnd_dir+'</p></div><div class="pression"><p>Pression: '+monObjet.current_condition.pressure+' bars</p></div></p></div>';
 
 
 //                      icones jour 1
@@ -36,7 +36,7 @@ $(document).ready(function(){
 
 //                      jour2
          var element2 = document.getElementById("jour2");
-         element2.innerHTML = '<div><img id="icon2" class="icons" src="'+monObjet.fcst_day_1.icon_big+'" alt="icon météo"><h1><div class="dayname">'+ monObjet.fcst_day_1.day_long +'</div><br><div class="date">'+ monObjet.fcst_day_1.date+'</div></h1><h2><div class="tempM"> Temps Min/Max: '+ monObjet.fcst_day_1.tmin +'°/'+ monObjet.fcst_day_1.tmax+'°'+'</h2><p class="condition">Condition: '+ monObjet.fcst_day_1.condition +'</p></div>';
+         element2.innerHTML = '<div class="joursui"><img id="icon2" class="icons" src="'+monObjet.fcst_day_1.icon_big+'" alt="icon météo"><h1><div class="dayname">'+ monObjet.fcst_day_1.day_long +'</div><br><div class="date">'+ monObjet.fcst_day_1.date+'</div></h1><h2><div class="tempM"> Temps Min/Max: '+ monObjet.fcst_day_1.tmin +'°/'+ monObjet.fcst_day_1.tmax+'°'+'</h2><p class="condition">Condition: '+ monObjet.fcst_day_1.condition +'</p></div>';
 
 
 //                      icones jour 2
@@ -52,7 +52,7 @@ $(document).ready(function(){
 
 //                      jour3
          var element3 = document.getElementById("jour3");
-         element3.innerHTML = '<div><img id="icon3" class="icons" src="'+monObjet.fcst_day_2.icon_big+'" alt="icon météo"><h1><div class="dayname">'+ monObjet.fcst_day_2.day_long +'</div><br><div class="date">'+ monObjet.fcst_day_2.date+'</div></h1><h2><div class="tempM"> Temps Min/Max: '+ monObjet.fcst_day_2.tmin +'°/'+ monObjet.fcst_day_2.tmax+'°'+'</h2><p class="condition">Condition: '+ monObjet.fcst_day_2.condition +'</p></div>';
+         element3.innerHTML = '<div class="joursui"><img id="icon3" class="icons" src="'+monObjet.fcst_day_2.icon_big+'" alt="icon météo"><h1><div class="dayname">'+ monObjet.fcst_day_2.day_long +'</div><br><div class="date">'+ monObjet.fcst_day_2.date+'</div></h1><h2><div class="tempM"> Temps Min/Max: '+ monObjet.fcst_day_2.tmin +'°/'+ monObjet.fcst_day_2.tmax+'°'+'</h2><p class="condition">Condition: '+ monObjet.fcst_day_2.condition +'</p></div>';
 
 
 //                      icones jour 3
@@ -67,7 +67,7 @@ $(document).ready(function(){
 
 //                      jour4
         var element4 = document.getElementById("jour4");
-        element4.innerHTML = '<div><img id="icon4" class="icons" src="'+monObjet.fcst_day_3.icon_big+'" alt="icon météo"><h1><div class="dayname">'+ monObjet.fcst_day_3.day_long +'</div><br><div class="date">'+ monObjet.fcst_day_3.date+'</div></h1><h2 class="tempM" Temps> Min/Max: '+ monObjet.fcst_day_3.tmin +'°/'+ monObjet.fcst_day_3.tmax+'°'+'</h2><p class="condition">Condition: '+ monObjet.fcst_day_3.condition +'</p></div>';
+        element4.innerHTML = '<div class="joursui"><img id="icon4" class="icons" src="'+monObjet.fcst_day_3.icon_big+'" alt="icon météo"><h1><div class="dayname">'+ monObjet.fcst_day_3.day_long +'</div><br><div class="date">'+ monObjet.fcst_day_3.date+'</div></h1><h2 class="tempM">  Temps Min/Max: '+ monObjet.fcst_day_3.tmin +'°/'+ monObjet.fcst_day_3.tmax+'°'+'</h2><p class="condition">Condition: '+ monObjet.fcst_day_3.condition +'</p></div>';
 
 
 //                      icones jour 4
@@ -82,7 +82,7 @@ $(document).ready(function(){
 
 //                      jour5
         var element5 = document.getElementById("jour5");
-        element5.innerHTML = '<div><img id="icon5" class="icons" src="'+monObjet.fcst_day_4.icon_big+'" alt="icon météo"><h1><div class="dayname">'+ monObjet.fcst_day_4.day_long +'</div><br><div class="date">'+ monObjet.fcst_day_4.date+'</div></h1><h2 class="tempM"> Temps Min/Max: '+ monObjet.fcst_day_4.tmin +'°/'+ monObjet.fcst_day_4.tmax+'°'+'</h2><p class="condition">Condition: '+ monObjet.fcst_day_4.condition +'</p></div>';
+        element5.innerHTML = '<div class="joursui"><img id="icon5" class="icons" src="'+monObjet.fcst_day_4.icon_big+'" alt="icon météo"><h1><div class="dayname">'+ monObjet.fcst_day_4.day_long +'</div><br><div class="date">'+ monObjet.fcst_day_4.date+'</div></h1><h2 class="tempM"> Temps Min/Max: '+ monObjet.fcst_day_4.tmin +'°/'+ monObjet.fcst_day_4.tmax+'°'+'</h2><p class="condition">Condition: '+ monObjet.fcst_day_4.condition +'</p></div>';
 
 //                      icones jour 5
         if (monObjet.fcst_day_4.condition_key=="ensoleille" || monObjet.fcst_day_4.condition_key=="eclaircies"||monObjet.fcst_day_4.condition_key=="averses-de-pluie-faible"||
